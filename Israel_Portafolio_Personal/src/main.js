432bfbd439b0ad158c45ac10a0c51aa4e2a8e72f
alejandro <alejandrox025@>
2026-05-05 20:01:04 -0500

    Configuracion de rutas y intalacion de libreria de bootstrap, bootstrap icon ademas de ulitizar ademas de crear las view principales

--- Israel_Portafolio_Personal/src/main.js
+++ Israel_Portafolio_Personal/src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//Importaciones de libreria de bootstrap y bootstrap icon
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+createApp(App).use(router).mount('#app')
